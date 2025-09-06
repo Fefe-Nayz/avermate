fb8635845dab6e7eacd5c7e0cad00d90b2440a23
nayzflux <55506519+nayzflux@users.noreply.github.com>
2025-09-06 13:34:14 +0200

    fix: duplicate scroll in drawer

--- apps/client/src/components/credenza/credenza-content-wrapper.tsx
+++ apps/client/src/components/credenza/credenza-content-wrapper.tsx
@@ -1,9 +1,15 @@
+"use client"
+
 import React from 'react'
 import { CredenzaContent } from '../ui/credenza'
+import { useMediaQuery } from '../ui/use-media-query'
+import { cn } from '@/lib/utils';
 
 export default function CredenzaContentWrapper({ children }: { children: React.ReactNode }) {
+    const isDesktop = useMediaQuery("(min-width: 768px)");
+
     return (
-        <CredenzaContent className="max-h-screen overflow-y-auto">
+        <CredenzaContent className={cn("max-h-screen", isDesktop && "overflow-y-scroll")}>
             {children}
         </CredenzaContent>
     )
