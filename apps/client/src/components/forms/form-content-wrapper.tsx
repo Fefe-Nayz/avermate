31c3ae14885a96c5b32f41e0ad259e4f86b0e76a
nayzflux <55506519+nayzflux@users.noreply.github.com>
2025-09-06 13:36:36 +0200

    chore: format

--- apps/client/src/components/forms/form-content-wrapper.tsx
+++ apps/client/src/components/forms/form-content-wrapper.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function FormContentWrapper({children}: {children: React.ReactNode}) {
+export default function FormContentWrapper({ children }: { children: React.ReactNode }) {
   return (
     <div className='flex flex-col lg:gap-8 gap-4'>
       {children}
