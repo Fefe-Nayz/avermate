c13057817464a648b39b02326dc5aed3070d957f
nayzflux <55506519+nayzflux@users.noreply.github.com>
2025-09-29 21:10:56 +0200

    feat: add grades table in subject details

---
+++ apps/client/src/components/tables/subject-grades-table.tsx
@@ -0,0 +1,295 @@
+"use client"
+
+import * as React from "react"
+import {
+    ColumnDef,
+    ColumnFiltersState,
+    flexRender,
+    getCoreRowModel,
+    getFilteredRowModel,
+    getPaginationRowModel,
+    getSortedRowModel,
+    SortingState,
+    useReactTable,
+    VisibilityState,
+} from "@tanstack/react-table"
+import { ArrowUpDown, ChevronDown, MoreHorizontal } from "lucide-react"
+
+import { Button } from "@/components/ui/button"
+import { Checkbox } from "@/components/ui/checkbox"
+import {
+    DropdownMenu,
+    DropdownMenuCheckboxItem,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Input } from "@/components/ui/input"
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table"
+import { PartialGrade } from "@/types/grade"
+import GradeBadge from "./grade-badge"
+import GradeMoreButton from "../buttons/dashboard/grade/grade-more-button"
+import { useFormatter, useTranslations } from "next-intl"
+import { useFormatDates } from "@/utils/format"
+import { format } from "path"
+
+export const columns: ColumnDef<PartialGrade>[] = [
+    // {
+    //     id: "select",
+    //     header: ({ table }) => (
+    //         <Checkbox
+    //             checked={
+    //                 table.getIsAllPageRowsSelected() ||
+    //                 (table.getIsSomePageRowsSelected() && "indeterminate")
+    //             }
+    //             onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+    //             aria-label="Select all"
+    //         />
+    //     ),
+    //     cell: ({ row }) => (
+    //         <Checkbox
+    //             checked={row.getIsSelected()}
+    //             onCheckedChange={(value) => row.toggleSelected(!!value)}
+    //             aria-label="Select row"
+    //         />
+    //     ),
+    //     enableSorting: false,
+    //     enableHiding: false,
+    // },
+    {
+        accessorKey: "name",
+        enableResizing: false,
+        header: ({ column }) => {
+            const t = useTranslations("Dashboard.Tables.SUBJECT_GRADES_TABLE");
+
+            return (
+                <Button
+                    variant="ghost"
+                    onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+                >
+                    {t("HEADER_NAME")}
+                    <ArrowUpDown />
+                </Button>
+            )
+        },
+        cell: ({ row }) => <p className="truncate text-ellipsis max-w-[160px] md:max-w-[300px] xl:max-w-full">{row.getValue("name")}
+        </p>,
+    },
+    {
+        accessorKey: "grade",
+        header: ({ column }) => {
+            const t = useTranslations("Dashboard.Tables.SUBJECT_GRADES_TABLE");
+
+            return (
+                <Button
+                    variant="ghost"
+                    onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+                >
+                    {t("HEADER_GRADE")}
+                    <ArrowUpDown />
+                </Button>
+            )
+        },
+        cell: ({ row }) => {
+            const { id, value, coefficient, periodId, subjectId, outOf } = row.original;
+
+            return (
+                <div>
+                    <GradeBadge id={id} value={value} coefficient={coefficient} periodId={periodId} subjectId={subjectId} outOf={outOf} />
+                </div>
+            )
+        }
+    },
+    // {
+    //     accessorKey: "passedAt",
+    //     header: ({ column }) => {
+    //         const t = useTranslations("Dashboard.Tables.SUBJECT_GRADES_TABLE");
+
+    //         return (
+    //             <Button
+    //                 variant="ghost"
+    //                 onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+    //             >
+    //                 {t("HEADER_PASSED_AT")}
+    //                 <ArrowUpDown />
+    //             </Button>
+    //         )
+    //     },
+    //     cell: ({ row }) => {
+    //         const formatter = useFormatter();
+    //         const format = useFormatDates(formatter);
+
+    //         return (
+    //             <div>{format.formatIntermediate(row.getValue("passedAt"))}</div>
+    //         );
+    //     },
+    // },
+    {
+        id: "actions",
+        enableHiding: false,
+        enableSorting: false,
+        cell: ({ row }) => {
+            const grade = row.original
+
+            return (
+                <div className="flex justify-end">
+                    <GradeMoreButton grade={grade} />
+                </div>
+            )
+        },
+    },
+]
+
+export function SubjectGradesTable({ grades }: { grades: PartialGrade[] }) {
+    const t = useTranslations("Dashboard.Tables.SUBJECT_GRADES_TABLE");
+    const [sorting, setSorting] = React.useState<SortingState>([])
+    const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
+        []
+    )
+    const [columnVisibility, setColumnVisibility] =
+        React.useState<VisibilityState>({})
+    const [rowSelection, setRowSelection] = React.useState({})
+
+    const table = useReactTable({
+        data: grades,
+        columns,
+        onSortingChange: setSorting,
+        onColumnFiltersChange: setColumnFilters,
+        getCoreRowModel: getCoreRowModel(),
+        getPaginationRowModel: getPaginationRowModel(),
+        getSortedRowModel: getSortedRowModel(),
+        getFilteredRowModel: getFilteredRowModel(),
+        onColumnVisibilityChange: setColumnVisibility,
+        onRowSelectionChange: setRowSelection,
+        state: {
+            sorting,
+            columnFilters,
+            columnVisibility,
+            rowSelection,
+        },
+    })
+
+    return (
+        <div className="w-full">
+            <div className="flex items-center py-4">
+                <Input
+                    placeholder={t("FILTER_NAME_PLACEHOLDER")}
+                    value={(table.getColumn("name")?.getFilterValue() as string) ?? ""}
+                    onChange={(event) =>
+                        table.getColumn("name")?.setFilterValue(event.target.value)
+                    }
+                    className="max-w-sm md:max-w-[40%]"
+                />
+                {/* <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button variant="outline" className="ml-auto">
+                            Columns <ChevronDown />
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end">
+                        {table
+                            .getAllColumns()
+                            .filter((column) => column.getCanHide())
+                            .map((column) => {
+                                return (
+                                    <DropdownMenuCheckboxItem
+                                        key={column.id}
+                                        className="capitalize"
+                                        checked={column.getIsVisible()}
+                                        onCheckedChange={(value) =>
+                                            column.toggleVisibility(!!value)
+                                        }
+                                    >
+                                        {column.id}
+                                    </DropdownMenuCheckboxItem>
+                                )
+                            })}
+                    </DropdownMenuContent>
+                </DropdownMenu> */}
+            </div>
+            <div className="overflow-hidden rounded-md border">
+                <Table>
+                    <TableHeader>
+                        {table.getHeaderGroups().map((headerGroup) => (
+                            <TableRow key={headerGroup.id}>
+                                {headerGroup.headers.map((header) => {
+                                    return (
+                                        <TableHead key={header.id}>
+                                            {header.isPlaceholder
+                                                ? null
+                                                : flexRender(
+                                                    header.column.columnDef.header,
+                                                    header.getContext()
+                                                )}
+                                        </TableHead>
+                                    )
+                                })}
+                            </TableRow>
+                        ))}
+                    </TableHeader>
+                    <TableBody>
+                        {table.getRowModel().rows?.length ? (
+                            table.getRowModel().rows.map((row) => (
+                                <TableRow
+                                    key={row.id}
+                                    data-state={row.getIsSelected() && "selected"}
+                                >
+                                    {row.getVisibleCells().map((cell) => (
+                                        <TableCell key={cell.id}>
+                                            {flexRender(
+                                                cell.column.columnDef.cell,
+                                                cell.getContext()
+                                            )}
+                                        </TableCell>
+                                    ))}
+                                </TableRow>
+                            ))
+                        ) : (
+                            <TableRow>
+                                <TableCell
+                                    colSpan={columns.length}
+                                    className="h-24 text-center"
+                                >
+                                    {t("FILTER_NO_RESULTS")}
+                                </TableCell>
+                            </TableRow>
+                        )}
+                    </TableBody>
+                </Table>
+            </div>
+            <div className="flex items-center justify-end space-x-2 py-4">
+                {/* <div className="text-muted-foreground flex-1 text-sm">
+                    {table.getFilteredSelectedRowModel().rows.length} of{" "}
+                    {table.getFilteredRowModel().rows.length} row(s) selected.
+                </div> */}
+                <div className="space-x-2">
+                    <Button
+                        variant="outline"
+                        size="sm"
+                        onClick={() => table.previousPage()}
+                        disabled={!table.getCanPreviousPage()}
+                    >
+                        {t("BUTTON_PREVIOUS")}
+                    </Button>
+                    <Button
+                        variant="outline"
+                        size="sm"
+                        onClick={() => table.nextPage()}
+                        disabled={!table.getCanNextPage()}
+                    >
+                        {t("BUTTON_NEXT")}
+                    </Button>
+                </div>
+            </div>
+        </div>
+    )
+}
